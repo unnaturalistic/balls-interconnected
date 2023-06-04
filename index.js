37a4e565b4c77e5114da733d1492de741a940b16
unnatural <karos2646@gmail.com>
2023-06-05 01:15:21 +0330

    kjhhjkjkhjkhjkhjhkjkh

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 9000
 var auth
 const db = new sqlite3.Database();
 
+db.serialize(() => {
+    db.run("CREATE TABLE tokens (tokens TEXT)");
+})
 
 app.listen(port, () => {
     console.log("Server started on port " + port)
@@ -21,8 +24,41 @@ app.get("revive", (req, res) => {
 })
 
 function Heartbeat() {
-
+    
 }
+
+function revivetoken(token) {
+    db.serialize(() => {
+        const stmt = db.prepare("INSERT INTO tokens VALUES (?)")
+        stmt.run(maketoken(12))
+        stmt.finalize();
+    })
+}
+
+function maketoken(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+function gettokens() {
+    var tokenlist = [];
+    let i = 0;
+
+    db.serialize(() => {
+        db.each("SELECT rowid AS id, token FROM tokens", (err, row) => {
+            tokenlist[0] = row.info
+            i++
+        })
+    })
+}
+
 /*
 db.serialize(() => {
     db.run("CREATE TABLE lorem (info TEXT)");
